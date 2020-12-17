b6e6188dc0057f4ff231fe7b29f287a4690031db
khrisyu9 <khrisyu@umich.edu>
2020-12-17 12:11:35 -0500

    added covid19 data preprocessing file

---
+++ covid19_datapreprocessing.R
@@ -0,0 +1,8 @@
+# load library
+library(rvest)
+library(dplyr)
+library(readr)
+
+# read page
+countyurl <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+countydata<-read_csv(url(countyurl))
