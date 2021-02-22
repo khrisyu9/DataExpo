31196f48cfd56953affe963f764152634400ecdf
khrisyu9 <khrisyu@umich.edu>
2021-02-22 09:59:01 -0500

    added new covid data

--- covidcast.R
+++ covidcast.R
@@ -1,7 +1,25 @@
 library(covidcast)
-cc_data <- suppressMessages(
-  covidcast_signal(data_source = "indicator-combination", signal = "confirmed_7dav_incidence_prop",
-                   start_day = "2021-01-15", end_day = "2021-02-15",
+
+#daily new COVID-19 Delphi's indicators combination at county level
+covid_indicator_combo <- suppressMessages(
+  covidcast_signal(data_source = "indicator-combination", signal = "nmf_day_doc_fbc_fbs_ght",
+                   start_day = "2020-03-01", end_day = "2020-12-31",
                    geo_type = "county")
 )
+write.csv(covid_indicator_combo, "D:/dataexpo/data/covidcast/covid_indicator_combo_20200301_20201231.csv")
 
+#daily new COVID-19 cases per 100k people at county level
+covid_cases <- suppressMessages(
+  covidcast_signal(data_source = "indicator-combination", signal = "confirmed_incidence_prop",
+                   start_day = "2020-03-01", end_day = "2020-12-31",
+                   geo_type = "county")
+)
+write.csv(covid_cases, "D:/dataexpo/data/covidcast/covid_cases_20200301_20201231.csv")
+
+#daily new COVID-19 cases per 100k people at county level
+covid_deaths <- suppressMessages(
+  covidcast_signal(data_source = "indicator-combination", signal = "confirmed_incidence_prop",
+                   start_day = "2020-03-01", end_day = "2020-12-31",
+                   geo_type = "county")
+)
+write.csv(covid_deaths, "D:/dataexpo/data/covidcast/covid_deaths_20200301_20201231.csv")
