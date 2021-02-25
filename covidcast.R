87854269945760aef2ee5668869efa9c6db4d9ab
khrisyu9 <khrisyu@umich.edu>
2021-02-24 22:22:21 -0500

    edited new data

--- covidcast.R
+++ covidcast.R
@@ -16,10 +16,11 @@ covid_cases <- suppressMessages(
 )
 write.csv(covid_cases, "D:/dataexpo/data/covidcast/covid_cases_20200301_20201231.csv")
 
-#daily new COVID-19 cases per 100k people at county level
+#daily death of COVID-19 cases per 100k people at county level
 covid_deaths <- suppressMessages(
   covidcast_signal(data_source = "indicator-combination", signal = "confirmed_incidence_prop",
                    start_day = "2020-03-01", end_day = "2020-12-31",
                    geo_type = "county")
 )
 write.csv(covid_deaths, "D:/dataexpo/data/covidcast/covid_deaths_20200301_20201231.csv")
+
