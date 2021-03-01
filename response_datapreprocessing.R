4645f9aa37088d5665333c4e197c138abf4133ca
khrisyu9 <khrisyu@umich.edu>
2021-03-01 01:00:54 -0500

    edited model loop

--- response_datapreprocessing.R
+++ response_datapreprocessing.R
@@ -18,6 +18,24 @@ covid_cases_new$geo_value <- as.factor(covid_cases_new$geo_value)
 
 covid_cases_new_ <- covid_cases_new[,c(1,4,7,10,11)]
 
+# apply spline models to all counties and save coefficients
+coef_1 <- 0
+coef_2 <- 0
+coef_3 <- 0
+coef_4 <- 0
+coef_5 <- 0
+coef_6 <- 0
+coef_7 <- 0
+coef_df <- data.frame(coef_1, coef_2, coef_3, coef_4, coef_5, coef_6, coef_7)
+
+county_geo <- unique(covid_cases_new_[c("geo_value")]) %>% pull(geo_value)
+
+for (i in 1:length(county_geo)){
+  county_data <- filter(covid_cases_new_, geo_value == county_geo[i])
+  fit <- lm(value ~ bs(time_value, df = 6), data = county_data)
+  coef_df <- rbind(coef_df, fit$coefficients)
+}
+
 covid_cases_by_county <- as_tibble(covid_cases_new_) %>%
   nest(-geo_value, -county_name, -state)
 
