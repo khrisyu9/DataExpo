f56de1b68f1540e9143ed2edad0a6c8a0679d3c7
khrisyu9 <khrisyu@umich.edu>
2021-03-22 00:14:41 -0400

    the bs model finally works

--- response_datapreprocessing.R
+++ response_datapreprocessing.R
@@ -1,4 +1,5 @@
 # load library
+library(plyr)
 library(rvest)
 library(tidyverse)
 library(readr)
@@ -16,24 +17,32 @@ covid_cases <- read.csv("D:/dataexpo/data/covidcast/covid_cases_20200301_2020123
 
 covid_cases_new <- merge(covid_cases, county_code, by = "geo_value")
 
-covid_cases_new$geo_value <- as.factor(covid_cases_new$geo_value)
+covid_cases_new_ <- covid_cases_new[,c(1,5,8,11,12)]
 
-covid_cases_new_ <- covid_cases_new[,c(1,4,7,10,11)]
+covid_cases_new_$geo_value <- as.character(covid_cases_new_$geo_value)
 
 # apply spline models to all counties and save coefficients
-coef_1 <- 0
-coef_2 <- 0
-coef_3 <- 0
-coef_4 <- 0
-coef_5 <- 0
-coef_6 <- 0
-coef_7 <- 0
-coef_df <- data.frame(coef_1, coef_2, coef_3, coef_4, coef_5, coef_6, coef_7)
+#coef_1 <- 0
+#coef_2 <- 0
+#coef_3 <- 0
+#coef_4 <- 0
+#coef_5 <- 0
+#coef_6 <- 0
+#coef_7 <- 0
+#coef_df <- data.frame(coef_1, coef_2, coef_3, coef_4, coef_5, coef_6, coef_7)
 
-county_geo <- unique(covid_cases_new_[c("geo_value")]) #%>% pull(geo_value)
+county_list <- unique(covid_cases_new_[c("geo_value")])
+
+covid_cases_new_$days <- as.numeric(julian(as.Date(covid_cases_new_$time_value), origin=as.Date("2020-03-01")))
+
+models <- dlply(covid_cases_new_, "geo_value", function(bycounty) 
+  lm(value ~ bs(days, df = 6), data = bycounty))
+
+# Apply coef to each model and return a data frame
+coef_df <- ldply(models, coef)
 
 for (i in 1:length(county_geo)){
-  county_data <- filter(covid_cases_new_, geo_value == county_geo[i])
+  county_data <- covid_cases_new_[which(covid_cases_new_$geo_value==county_geo[i]),]
   fit <- lm(value ~ bs(time_value, df = 6), data = county_data)
   coef_df <- rbind(coef_df, fit$coefficients)
 }
